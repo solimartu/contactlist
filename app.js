70e93cec2b860de7602be1a50e8dbe03596fa564
solimartu <soli13@gmail.com>
2022-02-21 13:11:37 +0100

    herokufirst

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use("/", indexRouter);
 app.use("/api/contacts", contactsRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
